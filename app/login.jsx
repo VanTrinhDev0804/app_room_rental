b6aa5d267351452081c1c2abb47f42ca98d7eb23
VanTrinhDev0804 <vantrinh2804@gmail.com>
2024-08-21 07:12:22 +0700

    first

--- app/login.jsx
+++ app/login.jsx
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ScreenWrapper from "../components/ScreenWrapper";
+import { theme } from "../constants/theme";
+import Icon from "../assets/icons";
+import { StatusBar } from "expo-status-bar";
+import BackButton from "../components/BackButton";
+import { useRouter } from "expo-router";
+import { hp, wp } from "../helpers/common";
+
+
 
 const login = () => {
-    return (
-        <View>
-            <Text>login</Text>
+
+  const router = useRouter()
+  return (
+    <ScreenWrapper>
+        <StatusBar style="dark"/>
+        <View style={styles.container}>
+          <BackButton router={router}/>
         </View>
-    )
-}
 
-export default login
+        {/* welcome */}
+        <View>
+          <Text style= {styles.welcomeText}>Chào mừng</Text>
+          <Text tyle= {styles.welcomeText}>Bạn trở lại</Text>
+        </View>
+    </ScreenWrapper>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default login;
+
+const styles = StyleSheet.create({
+  container:{
+    flex : 1, 
+    gap: 45, 
+    paddingHorizontal: wp(5)
+  },
+  welcomeText: {
+    color: theme.colors.text,
+    fontSize: hp(4),
+    fontWeight: theme.fonts.bold,
+},
+});
